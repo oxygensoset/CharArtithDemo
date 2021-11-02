6a3aabc6a77cb567ca251c0538e01a362915e5d8
oxygensoset <saveliy.burmistrov.99@mail.ru>
2021-11-02 15:49:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char ch;
+    ch = 'L';
+    System.out.println("значение ch изменения на " + ch);
+    ch++;
+    System.out.println(" значение ch изменилось на " + ch);
+    ch = 'Д';
+    ch--;
+    System.out.println("значение ch снова изминилось и равно " + ch);
+    } // main (String[])
+} // CharArtithDemo
